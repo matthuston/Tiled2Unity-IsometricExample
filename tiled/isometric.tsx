247096761d9587122801899fdea396aa48802df5
matthuston <matthuston@gmail.com>
2017-08-13 11:12:46 -0400

    - Initial commit, added Unity 2017.1 project and Tiled2Unity 1.0.12.3

---
+++ tiled/isometric.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="isometric" tilewidth="128" tileheight="192" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="64" source="tiles/floor_01.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="64">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="128" height="192" source="tiles/walls_01.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="128" height="192" source="tiles/walls_02.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="128" height="192" source="tiles/walls_03.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="128" height="192" source="tiles/walls_04.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="128" height="192" source="tiles/walls_05.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <image width="128" height="192" source="tiles/walls_06.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="64" y="192">
+    <polygon points="0,0 64,-32 0,-64 -64,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
